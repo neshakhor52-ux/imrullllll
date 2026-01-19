a5a91bf424ba2cf37a3bab17857d55cce851a392
neshakhor52-ux <neshakhor52@gmail.com>
2026-01-18 18:05:21 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -27,3 +27,17 @@ async function fetchPic(){
 Box.innerText='Error';
  }
 }
+async function forceDownload(url){
+  const res = await fetch(url);
+  const blob = await res.blob();
+  const blobUrl = window.URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = blobUrl;
+  a.download = "facebook_image.jpg";
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+
+  window.URL.revokeObjectURL(blobUrl);
+}
